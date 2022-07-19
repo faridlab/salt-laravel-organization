7bdb9df01cd3aa973f0f1fd5c7be5300be79342e
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-19 21:32:34 +0700

    feat: organization specialties migration

---
+++ database/migrations/2022_07_19_091408_create_organization_specialties_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_specialties', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('organization_id')->references('id')->on('organizations');
+            $table->string('name');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_specialties');
+    }
+};
