707802648ace6cbdf468e871c1a64bdb5ec6c4ad
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-19 21:38:01 +0700

    feat: Organization Specialties model

---
+++ src/Models/OrganizationSpecialties.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SaltOrganization\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class OrganizationSpecialties extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table provinces
+        'id',
+        'organization_id',
+        'name',
+    ];
+
+    protected $rules = array(
+        'organization_id' => 'required|string',
+        'name' => 'required|string',
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $structures = array();
+    protected $forms = array();
+
+    protected $searchable = array(
+        'organization_id',
+        'name',
+    );
+
+}
