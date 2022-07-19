2c6f16eab46cdfb797e7c11df96ec0350f2cbd32
Farid Hidayat <farid.hidayat@sagara.id>
2022-07-19 21:37:08 +0700

    feat: Industries model

---
+++ src/Models/Industries.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SaltOrganization\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class Industries extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table provinces
+        'id',
+        'name',
+    ];
+
+    protected $rules = array(
+        'name' => 'required|string'
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $structures = array();
+    protected $forms = array();
+
+    protected $searchable = array('name');
+
+    public function organizations() {
+        return $this->hasMany('SaltOrganization\Models\Organizations', 'organization_id', 'id');
+    }
+
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SaltLaravelOrganization\Tests;
+namespace SaltOrganization\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Illuminate\Foundation\Application;
@@ -22,7 +22,7 @@ abstract class TestCase extends OrchestraTestCase
     protected function getPackageProviders($app)
     {
         return [
-            \SaltLaravelOrganization\Providers\SaltLaravelOrganizationServiceProvider::class
+            \SaltOrganization\Providers\SaltOrganizationServiceProvider::class
         ];
     }
 }
