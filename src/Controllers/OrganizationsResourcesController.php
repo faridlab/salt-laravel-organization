1da338d8c298dbf08f733e16224dd01e8bc563ed
Farid Hidayat <e.faridhidayat@gmail.com>
2022-09-29 00:04:19 +0700

    feat: OrganizationsResourcesController

---
+++ src/Controllers/OrganizationsResourcesController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace SaltOrganization\Controllers;
+
+use OpenApi\Annotations as OA;
+
+use SaltLaravel\Controllers\Controller;
+use SaltLaravel\Controllers\Traits\ResourceIndexable;
+use SaltLaravel\Controllers\Traits\ResourceStorable;
+use SaltLaravel\Controllers\Traits\ResourceShowable;
+use SaltLaravel\Controllers\Traits\ResourceUpdatable;
+use SaltLaravel\Controllers\Traits\ResourcePatchable;
+use SaltLaravel\Controllers\Traits\ResourceDestroyable;
+use SaltLaravel\Controllers\Traits\ResourceTrashable;
+use SaltLaravel\Controllers\Traits\ResourceTrashedable;
+use SaltLaravel\Controllers\Traits\ResourceRestorable;
+use SaltLaravel\Controllers\Traits\ResourceDeletable;
+use SaltLaravel\Controllers\Traits\ResourceImportable;
+use SaltLaravel\Controllers\Traits\ResourceExportable;
+use SaltLaravel\Controllers\Traits\ResourceReportable;
+
+/**
+ * @OA\Info(
+ *      title="Organizations Endpoint",
+ *      version="1.0",
+ *      @OA\Contact(
+ *          name="Farid Hidayat",
+ *          email="farid@startapp.id",
+ *          url="https://startapp.id"
+ *      )
+ *  )
+ */
+class OrganizationsResourcesController extends Controller
+{
+    protected $modelNamespace = 'SaltOrganization';
+
+    /**
+     * @OA\Get(
+     *      path="/api/v1/organizations",
+     *      @OA\ExternalDocumentation(
+     *          description="More documentation here...",
+     *          url="https://github.com/faridlab/laravel-search-query"
+     *      ),
+     *      @OA\Parameter(
+     *          in="query",
+     *          name="search",
+     *          required=false
+     *      ),
+     *      @OA\Response(
+     *          response="200",
+     *          description="List of Organization"
+     *      ),
+     *      @OA\Response(response="default", description="Welcome page")
+     * )
+     */
+    use ResourceIndexable;
+
+    use ResourceStorable;
+    use ResourceShowable;
+    use ResourceUpdatable;
+    use ResourcePatchable;
+    use ResourceDestroyable;
+    use ResourceTrashable;
+    use ResourceTrashedable;
+    use ResourceRestorable;
+    use ResourceDeletable;
+    use ResourceImportable;
+    use ResourceExportable;
+    use ResourceReportable;
+}
+
